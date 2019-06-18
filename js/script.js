613f6118176c047b56a177d42dc6f79a086cdff6
Anugrah Ramadhan <dialog.anugrah@gmail.com>
2019-06-18 22:21:46 +0700

    ^^

--- js/script.js
+++ js/script.js
@@ -108,10 +108,12 @@ $('#surat-list').on('click', '.see-detail', function () {
 
         $.each(surat, function (i, data) {
             if (data.nomor == nomorSurat) {
+                $('.modal-title').html('')
+                $('.modal-body').html('')
                 title = data.nama + '&nbsp;(&nbsp;' + data.asma + '&nbsp;)'
                 tempat = data.type.toLowerCase().replace(/\b[a-z]/g, function (letter) {
                     return letter.toUpperCase();
-                });
+                })
 
                 detilSurat = `
                 <div class="container-fluid">
@@ -142,3 +144,41 @@ $('#surat-list').on('click', '.see-detail', function () {
     })
 })
 //end of lihat detail surat
+
+//search surat
+function searchSurat() {
+    let keyword = $('#search-input').val()
+    $.getJSON('https://al-quran-8d642.firebaseio.com/data.json?print=pretty', function (surat) {
+        
+        $.each(surat, function (i, data) {
+            if (surat.nama == keyword) {
+                $('#surat-list').html('')
+
+                hasilCari = `
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">` + data.nama + `&nbsp;(` + data.asma + `)</h5>
+                            <h6 class = "card-subtitle mb-2 text-muted"> ` + data.arti + ` </h6>
+                            <audio controls>
+                                <source src="` + data.audio + `" type="audio/mpeg">
+                                Your browser does not support the audio element.
+                            </audio>
+                            <br>
+                            <a href="#" class="card-link see-detail"  data-nomor="` + data.nomor + `"data-toggle="modal" data-target="#exampleModal">Detail Surat</a>
+                        </div>
+                    </div>`
+            }
+        })
+        $('#surat-list').html(hasilCari)
+    })
+}
+
+$('#search-button').on('click', function () {
+    searchSurat()
+})
+
+$('#search-input').on('keyup', function (e) {
+    if (e.which === 13) {
+        searchSurat()
+    }
+})
